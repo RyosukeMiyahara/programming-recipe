ee61ada2d893f1265662e22bf8c4bfd08837c115
Ryosuke Miyahara <r_panda007@yahoo.co.jp>
2014-12-27 23:55:39 +0900

    Added php version

---
+++ recipes/show_current_local_time/php/jp/RESULT.md
@@ -0,0 +1,2 @@
+$ php show_current_local_time.php
+2014/12/27 23:51:13
---
+++ recipes/show_current_local_time/php/show_current_local_time.php
@@ -0,0 +1,7 @@
+<?php
+// Set time zone
+date_default_timezone_set('Asia/Tokyo');
+
+// Show current time
+echo date("Y/m/d H:i:s\r\n");
+?>
