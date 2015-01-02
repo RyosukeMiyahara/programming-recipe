b13ae38bca867dd58c4f7152098810209b7674ef
RyosukeMiyahara <r_panda007@yahoo.co.jp>
2015-01-02 13:33:05 +0000

    Added new theme get_absolute_value

---
+++ recipes/get_absolute_value/c/get_absolute_value.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+  // Get absolute value of integer
+  printf("Absolute value of  5 is %d\n", abs(5));
+  printf("Absolute value of -5 is %d\n", abs(-5));
+
+  // Get absolute value of float
+  printf("Absolute value of  5.5 is %f\n", fabs(5.5));
+  printf("Absolute value of -5.5 is %f\n", fabs(-5.5));
+
+  return 0;
+}
