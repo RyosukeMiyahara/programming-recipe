0841310f6c182ab53f087ea200161da34715a8ef
pandineer <pandineer@gmail.com>
2015-01-03 17:22:34 +0900

    Added get_absolute_value in golang

---
+++ recipes/get_absolute_value/go/get_absolute_value.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	// Get absolute value of integer
+	fmt.Println("Absolute value of  5 is ", math.Abs(5))
+	fmt.Println("Absolute value of -5 is ", math.Abs(-5))
+
+	// Get absolute value of floag
+	fmt.Println("Absolute value of  5.5 is ", math.Abs(5.5))
+	fmt.Println("Absolute value of -5.5 is ", math.Abs(-5.5))
+}
