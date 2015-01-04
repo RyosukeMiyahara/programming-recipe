cacd822befb0f2e0b0e4e71b77445a6e366e8baa
RyosukeMiyahara <r_panda007@yahoo.co.jp>
2015-01-04 23:20:39 +0900

    Added get_absolute_value in php

---
+++ recipes/get_absolute_value/php/get_absolute_value.php
@@ -0,0 +1,9 @@
+<?php
+// Get absolute value of integer
+echo "Absolute value of  5 is ".abs(5)."\n";
+echo "Absolute value of -5 is ".abs(-5)."\n";
+
+// Get absolute value of float
+echo "Absolute value of  5.5 is ".abs(5.5)."\n";
+echo "Absolute value of -5.5 is ".abs(-5.5)."\n";
+?>
