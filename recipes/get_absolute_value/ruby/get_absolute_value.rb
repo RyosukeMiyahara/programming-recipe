d7e6eff239f21d4ce385fec6ef57edb8ca8fb7bc
RyosukeMiyahara <r_panda007@yahoo.co.jp>
2015-01-05 14:36:31 +0000

    Added get_absolute_value in ruby

---
+++ recipes/get_absolute_value/ruby/get_absolute_value.rb
@@ -0,0 +1,7 @@
+# Get absolute value of integer
+print("Absolute value of  5 is ", 5.abs, "\n")
+print("Absolute value of -5 is ", (-5).abs, "\n")
+
+# Get absolute value of float
+print("Absolute value of  5.5 is ", 5.5.abs, "\n")
+print("Absolute value of -5.5 is ", (-5.5).abs, "\n")
