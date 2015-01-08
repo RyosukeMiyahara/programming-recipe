f6fba5cbf92f6f8e818b1f5c4ba4803b2821474e
Ryosuke Miyahara <r_panda007@yahoo.co.jp>
2015-01-08 22:54:42 +0900

    Added comment in php

---
+++ recipes/comment/php/comment.php
@@ -0,0 +1,13 @@
+<?php
+// This is one line comment
+echo "One line comment can be written after statement\n"; // This is one line comment
+# This is one line comment
+echo "One lin comment can be written with // and #\n"; # This is one line comment
+/* This comment style can be written in multi lines like the following */
+/*
+  line 1
+  line 2
+*/
+/* This comment style can be embedded in statement */
+echo "This echo statement line includes " /* comment */. "comment\n";
+?>
