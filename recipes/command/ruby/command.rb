e4b8c8be11311f3d0120bef1b619917832472a48
ryosuke <r_m_panda@yahoo.co.jp>
2015-06-08 11:48:59 +0000

    Added ruby case for command

---
+++ recipes/command/ruby/command.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/ruby
+`touch panda.txt`
+
+system("touch giant_panda.txt")
+
+exec("touch cute_panda.txt")
---
+++ recipes/command/ruby/jp/RESULT.md
@@ -0,0 +1,5 @@
+$ ls
+command.rb
+$ ruby command.rb
+$ ls
+command.rb  cute_panda.txt  giant_panda.txt  panda.txt
