0d42ae7d42303303925d35b1d7a7f0535c49664c
ryosuke <r_m_panda@yahoo.co.jp>
2015-06-15 11:51:16 +0000

    Added cpp case for for_loop

---
+++ recipes/for_loop/cpp/for_loop.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(void) {
+  for (int i = 0; i < 5; i++) {
+    std::cout << "loop count = " << i << std::endl;
+  }
+  return 0;
+}
---
+++ recipes/for_loop/cpp/jp/RESULT.md
@@ -0,0 +1,7 @@
+$ g++ for_loop.cpp -o for_loop
+$ ./for_loop
+loop count = 0
+loop count = 1
+loop count = 2
+loop count = 3
+loop count = 4
