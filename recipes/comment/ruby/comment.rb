9f127d38b59069f1997931f59635cd62566fd1f1
ryosuke <r_panda007@yahoo.co.jp>
2015-12-16 08:34:46 +0900

    Added comment for ruby

---
+++ recipes/comment/ruby/comment.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+# one line comment 
+
+puts "one line comment can be written after statement" # one line comment
+
+=begin
+This comment style can be written in multi lines like this
+ line 1
+ line 2
+
+!!! NOTE !!!
+"=begin" and "=end" must be written at beginning of line
+=end
