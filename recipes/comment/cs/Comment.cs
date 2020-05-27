cebc5112bc4adc10e6025680361645abb49b718b
pandineer <pandineer@gmail.com>
2020-05-27 22:02:06 +0900

    write comment cs jp code, but not executed yet

---
+++ recipes/comment/cs/Comment.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Comment {
+    static void Main() {
+        // one line comment
+	Console.WriteLine("one line comment can write after statement\n"); // one line comment
+
+	/* This comment style can be written in multi lines like the following */
+	/*
+	    line 1
+	    line 2
+	*/
+	/* this comment style can be embedded in statement */
+	Console.WriteLine("This WriteLine statement line includes " /* comment */ "comment\n");
+	}
+}
