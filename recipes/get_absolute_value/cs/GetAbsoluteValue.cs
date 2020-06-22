78e4873c719daca6cce56a7d4e0a41235c938303
pandineer <pandineer@gmail.com>
2020-06-22 20:16:01 +0900

    implement code to get absolute value of -5 in cs

--- recipes/get_absolute_value/cs/GetAbsoluteValue.cs
+++ recipes/get_absolute_value/cs/GetAbsoluteValue.cs
@@ -3,6 +3,7 @@ using System;
 class GetAbsoluteValue {
     static void Main() {
         // Get absolute value of integer
-	Console.WriteLine("Absolute value of 5 is " + System.Math.Abs(5));
+	Console.WriteLine("Absolute value of  5 is " + System.Math.Abs(5));
+	Console.WriteLine("Absolute value of -5 is " + System.Math.Abs(-5));
     }
 }
