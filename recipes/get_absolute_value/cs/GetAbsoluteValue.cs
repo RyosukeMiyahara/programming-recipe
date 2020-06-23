bcdeaf5400a9ad48525f6ead6a3ed4bac7cdedde
pandineer <pandineer@gmail.com>
2020-06-23 21:09:00 +0900

    implement code to get absolute value of 5.5 in cs

--- recipes/get_absolute_value/cs/GetAbsoluteValue.cs
+++ recipes/get_absolute_value/cs/GetAbsoluteValue.cs
@@ -5,5 +5,8 @@ class GetAbsoluteValue {
         // Get absolute value of integer
 	Console.WriteLine("Absolute value of  5 is " + System.Math.Abs(5));
 	Console.WriteLine("Absolute value of -5 is " + System.Math.Abs(-5));
+
+        // Get absolute value of decimal
+	Console.WriteLine("Absolute value of  5.5 is " + System.Math.Abs(5.5));
     }
 }
