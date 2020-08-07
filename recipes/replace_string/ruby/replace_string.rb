b227754daad67fd3cc5c7c2754e5972664c7cb48
pandineer <pandineer@gmail.com>
2020-08-07 23:28:36 +0900

    gsub for multiple times

--- recipes/replace_string/ruby/replace_string.rb
+++ recipes/replace_string/ruby/replace_string.rb
@@ -1,3 +1,6 @@
 # Replace "Super" to "Great"
 p 'Super panda'.gsub(/Super/, 'Great')
 p 'Super panda'.gsub('Super', 'Great')
+
+# Replace multiple "Super" to "Great"
+p 'Super Super panda'.gsub(/Super/, 'Great')
